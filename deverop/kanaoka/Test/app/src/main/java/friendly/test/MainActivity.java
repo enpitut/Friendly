71190df798118387b96a6a19dfad39438c975c0d
Yuna Kanaoka <yuuna1227@ezweb.ne.jp>
2015-08-28 14:35:40 +0900

    08/28
    途中経過

--- deverop/kanaoka/Test/app/src/main/java/friendly/test/MainActivity.java
+++ deverop/kanaoka/Test/app/src/main/java/friendly/test/MainActivity.java
@@ -4,16 +4,28 @@ import java.util.Calendar;
 
 import android.app.Activity;
 import android.app.TimePickerDialog;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
 public class MainActivity extends Activity {
 
+    private EditText editText;
+    private TextView textWrite, textRead;
+
+    private SharedPreferences dataStore;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // "DataStore"という名前でインスタンスを生成
+        SharedPreferences dataStore = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor ed = dataStore.edit();
+
         // TextViewインスタンスを取得(結果表示用)
         final TextView textview = (TextView) findViewById(R.id.textview);
         // カレンダーインスタンスを取得
@@ -25,9 +37,14 @@ public class MainActivity extends Activity {
                                           int minute) {
                         // セットされた時刻を取得してtextviewに反映
                         textview.setText("設定時刻は " + hourOfDay + ":" + minute + " です");
+
+                        // 入力文字列を"input"に書き込むSharedPreferences.
+                        ed.putInt("input", hourOfDay * 60 + minute);
+                        ed.commit();
                     }
                 }, date.get(Calendar.HOUR_OF_DAY), date.get(Calendar.MINUTE),
                 true);
+
         // タイトルをセット
         timePickerDialog.setTitle("時刻設定");
         // ダイアログを表示
