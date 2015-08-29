8b54f2c21fb217384d09889ac08ba4d74a0db8a1
Oz-Soma <aaka2744@chiba-u.jp>
2015-08-30 04:39:34 +0900

    画像の描画、重畳表示

--- deverop/kanaoka/Test/app/src/main/java/friendly/test/MainActivity.java
+++ deverop/kanaoka/Test/app/src/main/java/friendly/test/MainActivity.java
@@ -3,25 +3,31 @@ package friendly.test;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.ImageView;
+import android.view.ViewGroup;
 
 public class MainActivity extends Activity{
 
     private int minute = 1;
+
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     // TextViewインスタンスを取得(結果表示用)
         final TextView textview = (TextView) findViewById(R.id.textview);
 
-        setContentView(R.layout.activity_main);
-        SettingTime settingtime = new SettingTime();
-         minute = settingtime.set_time();
+
+       ImageView img = (ImageView)findViewById(R.id.image);
+       setContentView(R.layout.activity_main);
+
+       // SettingTime settingtime = new SettingTime();
+       //  minute = settingtime.set_time();
 
         // セットされた時刻を取得してtextviewに反映
-        textview.setText(String.format("設定時刻は%02d:%02dです",minute/60,minute - 60*(minute/60)));
+       // textview.setText(String.format("設定時刻は%02d:%02dです",minute/60,minute - 60*(minute/60)));
 
-        SavingData savingdata= new SavingData();
-        savingdata.Save(minute);
+       // SavingData savingdata= new SavingData();
+       // savingdata.Save(minute);
 
     }
     public void onStart(){
